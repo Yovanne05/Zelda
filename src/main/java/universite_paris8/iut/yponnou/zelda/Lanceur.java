75843062d9f8c53347f342f992405d5c1cb78f20
Yovanne Ponnou <yovanne.ponnou@gmail.com>
2024-04-30 15:02:16 +0200

    V1

--- src/main/java/universite_paris8/iut/yponnou/zelda/Lanceur.java
+++ src/main/java/universite_paris8/iut/yponnou/zelda/Lanceur.java
@@ -7,17 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
+
+public class Lanceur extends Application {
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Lanceur.class.getResource("village.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1500, 900);
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
