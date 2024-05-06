5aaab125b761668f01bfe0139238a6f0a7a4b351
Yovanne05 <yovanne.ponnou@gmail.com>
2024-05-06 09:43:17 +0200

    Test commit

--- src/main/java/universite_paris8/iut/yponnou/zelda/village/Monde.java
+++ src/main/java/universite_paris8/iut/yponnou/zelda/village/Monde.java
@@ -18,7 +18,7 @@ public class Monde {
     public void creationMap() {
         for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab[i].length; j++) {
-                if (i%2==0 || j%4==0) {
+                if (i%7==0 || j%8==0) {
                     tab[i][j]=0;
                 }else {
                     tab[i][j]=1;
