cfa8b2d88abbe6b8647ff27b60594ed4fef9cbd6
Yovanne Ponnou <yovanne.ponnou@gmail.com>
2024-05-06 09:14:22 +0200

    Map

--- src/main/java/universite_paris8/iut/yponnou/zelda/Controleur.java
+++ src/main/java/universite_paris8/iut/yponnou/zelda/Controleur.java
@@ -1,4 +1,42 @@
 package universite_paris8.iut.yponnou.zelda;
 
-public class Controleur {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import universite_paris8.iut.yponnou.zelda.village.Monde;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controleur implements Initializable {
+    private Monde monde;
+    @FXML
+    private Pane pane;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        monde =new Monde(30,30);
+        monde.creationMap();
+        afficheMonde();
+    }
+
+    public void afficheMonde(){
+        int[][] tab=monde.getTab();
+        for(int i=0;i<tab.length;i++){
+            for (int j=0;j<tab[i].length;j++){
+                Rectangle rectangle = new Rectangle(50,50);
+                rectangle.setX(i*50);
+                rectangle.setY(j*50);
+                if(tab[i][j]==1){
+                    rectangle.setFill(Color.BLACK);
+                }
+                else{
+                    rectangle.setFill(Color.RED);
+                }
+                pane.getChildren().add(rectangle);
+            }
+        }
+    }
+
 }
---
+++ src/main/java/universite_paris8/iut/yponnou/zelda/village/Monde.java
@@ -0,0 +1,31 @@
+package universite_paris8.iut.yponnou.zelda.village;
+
+import java.util.Random;
+
+public class Monde {
+
+
+    private int[][] tab;
+
+    public Monde(int h, int l) {
+        this.tab = new int[h][l];
+    }
+
+    public int[][] getTab() {
+        return tab;
+    }
+
+    public void creationMap() {
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                if (i%5==0 || j%13==0) {
+                    tab[i][j]=0;
+                }else {
+                    tab[i][j]=1;
+            }
+        }
+    }
+
+
+}
+}
