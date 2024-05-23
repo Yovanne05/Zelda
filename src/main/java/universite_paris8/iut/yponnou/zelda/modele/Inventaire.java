046dba19161f8366c2c21c7b67d8aeaa3119f310
Yovanne05 <yovanne.ponnou@gmail.com>
2024-05-23 15:46:00 +0200

    - Ajout attaque avec eppe du héro (pas encore oppérationnel)

---
+++ src/main/java/universite_paris8/iut/yponnou/zelda/modele/Inventaire.java
@@ -0,0 +1,29 @@
+package universite_paris8.iut.yponnou.zelda.modele;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Inventaire{
+
+    private final ObservableList<Objet> objets = FXCollections.observableArrayList();
+    private final int taille;
+
+    public Inventaire(int capaciteStockage) {
+        taille = capaciteStockage;
+    }
+
+    public ObservableList<Objet> getObjets() {
+        return objets;
+    }
+    public int getTaille() {
+        return taille;
+    }
+
+    public void ajouterObjet(Objet objet) {
+        objets.add(objet);
+    }
+    public void deposerObjet(Objet objet) {
+        objets.remove(objet);
+    }
+
+}
