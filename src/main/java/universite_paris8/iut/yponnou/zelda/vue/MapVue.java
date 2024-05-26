adb66226b0fac01a1a3418ad416c3b5b4a7d2a1c
Mazur123456 <jakubmazur1356@gmail.com>
2024-05-26 19:15:03 +0200

    ajout des images des routes

--- src/main/java/universite_paris8/iut/yponnou/zelda/vue/MapVue.java
+++ src/main/java/universite_paris8/iut/yponnou/zelda/vue/MapVue.java
@@ -30,6 +30,23 @@ public class MapVue extends Affichable {
     private Image water12;
     private Image water13;
 
+
+    private Image road0;
+    private Image road1;
+    private Image road2;
+    private Image road3;
+    private Image road4;
+    private Image road5;
+    private Image road6;
+    private Image road7;
+    private Image road8;
+    private Image road9;
+    private Image road10;
+    private Image road11;
+    private Image road12;
+
+
+
     private List<Rectangle> obstaclesHitboxes;
 
 
@@ -54,6 +71,26 @@ public class MapVue extends Affichable {
         water11= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\water\\water11.png");
         water12= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\water\\water12.png");
         water13= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\water\\water13.png");
+
+
+        road0= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road00.png");
+        road1= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road01.png");
+        road2= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road02.png");
+        road3= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road03.png");
+        road4= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road04.png");
+        road5= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road05.png");
+        road6= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road06.png");
+        road7= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road07.png");
+        road8= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road08.png");
+        road9= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road09.png");
+        road10= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road10.png");
+        road11= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road11.png");
+        road12= new Image("file:src\\main\\resources\\universite_paris8\\iut\\yponnou\\zelda\\textures\\road\\road12.png");
+
+
+
+
+
 
 
         obstaclesHitboxes = new ArrayList<>();
@@ -102,7 +139,34 @@ public class MapVue extends Affichable {
                 }else if (getTab()[y][x] == 33) {
                     imageView = new ImageView(water13);
                 }
-// HOUSE
+// ROAD
+                else if (getTab()[y][x] == 1) {
+                    imageView = new ImageView(road0);
+                } else if (getTab()[y][x] == 2) {
+                    imageView = new ImageView(road1);
+                }else if (getTab()[y][x] == 3) {
+                    imageView = new ImageView(road2);
+                } else if (getTab()[y][x] == 4) {
+                    imageView = new ImageView(road3);
+                }else if (getTab()[y][x] == 5) {
+                    imageView = new ImageView(road4);
+                } else if (getTab()[y][x] == 6) {
+                    imageView = new ImageView(road5);
+                }else if (getTab()[y][x] == 7) {
+                    imageView = new ImageView(road6);
+                } else if (getTab()[y][x] == 8) {
+                    imageView = new ImageView(road7);
+                }else if (getTab()[y][x] == 9) {
+                    imageView = new ImageView(road8);
+                }else if (getTab()[y][x] == 10) {
+                    imageView = new ImageView(road9);
+                }else if (getTab()[y][x] == 11) {
+                    imageView = new ImageView(road10);
+                }else if (getTab()[y][x] == 12) {
+                    imageView = new ImageView(road11);
+                }else if (getTab()[y][x] == 12) {
+                    imageView = new ImageView(road12);
+                }
 
 
 
