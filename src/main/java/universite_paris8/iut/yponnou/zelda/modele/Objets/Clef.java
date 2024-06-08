d8b655d76a0a50d9caa3b53f24178dc99719492f
Lucas Delacroix <lucas.delacroix6@gmail.com>
2024-06-08 14:15:12 +0200

    ajout des classes Clef et Porte (pas finaliser)

---
+++ src/main/java/universite_paris8/iut/yponnou/zelda/modele/Objets/Clef.java
@@ -0,0 +1,18 @@
+package universite_paris8.iut.yponnou.zelda.modele.Objets;
+
+import universite_paris8.iut.yponnou.zelda.modele.Environnement;
+
+public class Clef extends Objet {
+
+    private boolean etat = true;
+
+    public Clef(String id,double x, double y, Environnement environnement) {
+        super(x, y, environnement);
+        setId("Clef".concat(id));
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
--- src/main/java/universite_paris8/iut/yponnou/zelda/modele/Objets/Objet.java
+++ src/main/java/universite_paris8/iut/yponnou/zelda/modele/Objets/Objet.java
@@ -6,7 +6,7 @@ import universite_paris8.iut.yponnou.zelda.modele.Environnement;
 public abstract class Objet {
 
     private String nom;
-    private final String id;
+    private String id;
     private static int incremente = 0;
     private final Position position;
 
@@ -23,6 +23,9 @@ public abstract class Objet {
     }
     public String getId() {
         return id;
+    }
+    public void setId(String id) {
+        this.id = id;
     }
     public Position getPosition() {
         return position;
