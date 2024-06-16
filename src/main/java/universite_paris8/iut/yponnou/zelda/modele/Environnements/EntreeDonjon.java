cd8365ba623094c700bd44cd76ab7ff7e615041f
Yovanne <yovanne.ponnou@gmail.com>
2024-06-16 20:45:22 +0200

    Final

---
+++ src/main/java/universite_paris8/iut/yponnou/zelda/modele/Environnements/EntreeDonjon.java
@@ -0,0 +1,22 @@
+package universite_paris8.iut.yponnou.zelda.modele.Environnements;
+
+import universite_paris8.iut.yponnou.zelda.modele.Acteurs.Hero;
+import universite_paris8.iut.yponnou.zelda.modele.Map;
+
+public class EntreeDonjon extends Environnement{
+    public EntreeDonjon(Hero hero) {
+        super(new Map(30, 30),hero);
+        this.getMap().initialisationMapEntreeDonjon();
+    }
+
+    @Override
+    public void creationEnvironnement() {
+        getHero().changeEnvObjets(this);
+        getHero().getPosition().setEnv(this);
+
+        getHero().setDx(0);
+        getHero().setDy(0);
+        this.ajouterActeur(getHero());
+
+    }
+}
---
+++ src/main/java/universite_paris8/iut/yponnou/zelda/modele/Objets/Objet.java
@@ -0,0 +1,36 @@
+package universite_paris8.iut.yponnou.zelda.modele.Objets;
+
+import universite_paris8.iut.yponnou.zelda.Position;
+import universite_paris8.iut.yponnou.zelda.modele.Environnements.Environnement;
+
+public abstract class Objet {
+
+    private String nom;
+    private String id;
+    private static int incremente = 0;
+    private final Position position;
+
+    public Objet(double x, double y, Environnement environnement) {
+        position = new Position(x,y,environnement);
+        id = "Ob"+incremente++;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public Position getPosition() {
+        return position;
+    }
+
+    public abstract String toString();
+    
+}
